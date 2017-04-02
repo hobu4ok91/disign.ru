97bf1f56aa42a3ec6b998abcd16a1917baaa4a56
hobu4ok91 <serjvid91@mail.ru>
2017-04-02 14:09:49 +1000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		cleanCSS    = require('gulp-clean-css'),
+		rename       = require('gulp-rename'),
+		browserSync  = require('browser-sync').create(),
+		concat       = require('gulp-concat'),
+		gcmq =		   require('gulp-group-css-media-queries');
+		uglify       = require('gulp-uglify');
+		
+var grid = require('smart-grid');
+var settings = {
+    outputStyle: 'scss', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: "30px", /* gutter width px || % */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            'width': '1100px', /* -> @media (max-width: 1100px) */
+            'fields': '30px' /* side fields */
+        },
+        md: {
+            'width': '960px',
+            'fields': '15px'
+        },
+        sm: {
+            'width': '780px',
+            'fields': '15px'
+        },
+        xs: {
+            'width': '560px',
+            'fields': '15px'
+        }
+    }
+};
+
+gulp.task('smart', function () {
+    grid('./smart-grid', settings);
+});
+
+gulp.task('browser-sync', ['styles', 'scripts'], function() {
+		browserSync.init({
+				proxy: 'disign.ru',
+				notify: false
+		});
+});
+
+gulp.task('styles', function () {
+	return gulp.src('sass/main.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gcmq())
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'./app/libs/modernizr/modernizr.js',
+		'./app/libs/jquery/jquery-1.11.2.min.js',
+		'./app/libs/waypoints/waypoints.min.js',
+		'./app/libs/animate/animate-css.js',
+		])
+		.pipe(concat('libs.js'))
+		// .pipe(uglify()) //Minify libs.js
+		.pipe(gulp.dest('./app/js/'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('sass/*.scss', ['styles']);
+	gulp.watch('app/libs/**/*.js', ['scripts']);
+	gulp.watch('sass/**/*.scss').on("change", browserSync.reload);
+	gulp.watch('app/js/*.js').on("change", browserSync.reload);
+	gulp.watch('app/*.html').on('change', browserSync.reload);
+	gulp.watch('app/**/*.php').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
